2410d26e959fc60acbdf7432423be91372d95ea8
drtrifle <drtrifle@gmail.com>
2019-04-17 21:10:37 +0800

    Added Base Classes

---
+++ FSM Test/FSM Test/BaseState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class BaseState {
+    virtual void handleInput() {
+
+    }
+    
+    virtual void update() {
+
+    }
+
+};
---
+++ FSM Test/FSM Test/Hero.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Hero {
+private:
+
+public:
+    //Input is an int for now
+    virtual void handleInput(int input) {
+    };
+
+    virtual void update() {
+
+    };
+
+};
