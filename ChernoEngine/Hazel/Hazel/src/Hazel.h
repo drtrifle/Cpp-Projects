8d7bb8d8f3b0e7b1745d95f4355124fde5364cd8
KenWhy <drtrifle@gmail.com>
2019-04-22 23:43:33 +0800

    Setup basic Application and Entry Point

---
+++ ChernoEngine/Hazel/Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Only intended to be included by outside applications using the engine
+
+#include "Hazel/Application.h"
+
+// --- Entry Point ---
+#include "Hazel/EntryPoint.h"
+// --- --- --- --- ---
---
+++ ChernoEngine/Hazel/Hazel/src/Hazel/Core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+    //Shortcut instead of typing __declspec import/export
+    #ifdef HZ_BUILD_DLL
+        #define HAZEL_API __declspec(dllexport)
+    #else
+        #define HAZEL_API __declspec(dllimport)
+    #endif
+#else
+    #error  Hazel only Supports Windows!
+
+#endif
